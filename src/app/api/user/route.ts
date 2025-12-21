8b0869cf26e4af59868bf6ce5b70989b8885038e
enMista <oluwa.damilola246@gmail.com>
2025-12-20 22:05:06 -0500

    Fixed error 500 code

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { db } from '@/lib/db'
-import { hash } from 'bcryptjs' 
+import { hash } from 'bcrypt' 
 
     export async function POST(req: Request) {
         try {
@@ -25,18 +25,22 @@ import { hash } from 'bcryptjs'
                 return NextResponse.json({ user: null, message: 'User with this name already exists' }, {status: 409})
             }
 
-            const hashedPassword = await 
+            const hashedPassword = await hash(password, 10); //encrypt the password before anything else! 
 
             //create new user if there isn't already account w/ username or email
             const newUser = await db.user.create({
                 data: {
                     username,
                     email,
-                    password
+                    password: hashedPassword //
                 }
             })
+            //and ":" extracts the password property to newUserPassword and ...rest collects all remaining properties into a new object. 
+            const { password:newUserPassword, ...rest} = newUser //remove password from response sent back to us 
 
-        } catch (error) {
-
+          return NextResponse.json({ user: rest, message: 'User created successfully'}, { status: 201 })
+        } catch (error) { //if you don't have the catch error block, the code will err 500 internal server issue. 
+            console.error('Error creating user:', error)
+            return NextResponse.json({ user: null, message: 'An error occurred while creating user' }, { status: 500 })
         }
     }
