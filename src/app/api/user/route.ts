b5e10a6c1205d9c59b26145829bffa5616330ed4
enMista <oluwa.damilola246@gmail.com>
2025-12-23 21:56:02 -0500

    api integration sign-up part 1 i think no password confirmed

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,23 @@
 import { NextResponse } from 'next/server'
 import { db } from '@/lib/db'
 import { hash } from 'bcrypt' 
+import * as z from 'zod'
+
+//Define schema (shape) the POST request body should have for input validation 
+const userSchema = z
+  .object({
+    username: z.string().min(1, 'Username is required').max(100),
+    email: z.string().min(1, 'Email is required').email('Invalid email'),
+    password: z
+      .string()
+      .min(1, 'Password is required')
+      .min(8, 'Password must have than 8 characters'),
+  })
 
     export async function POST(req: Request) {
-        try {
-            const body = await req.json();
-            const { email, username, password } = body; 
+        try { //server reads req.json() 
+            const body = await req.json(); //validating it exists and correct shape
+            const { email, username, password } = userSchema.parse(body); //destructure after validating with userSchema
             
             //check if email already exists
             const existingUserByEmail = await db.user.findUnique({
