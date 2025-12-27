8330d232d7c7442580ab9cc18be65df8e802840e
enMista <oluwa.damilola246@gmail.com>
2025-12-27 02:12:02 -0500

    serverSession api route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar';
 import { Toaster } from '@/components/ui/toaster';
 import '@/styles/globals.css';
+import Provider from '@/components/Provider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
@@ -19,11 +20,14 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <main className='h-screen flex flex-col justify-center items-center'>
-          <Navbar />
-          {children}
-        </main>
-        <Toaster />
+        <Provider>
+          <main className='h-screen flex flex-col justify-center items-center'>
+            <Navbar />
+            {children}
+          </main>
+          <Toaster />
+        </Provider>
+
       </body>
     </html>
   );
