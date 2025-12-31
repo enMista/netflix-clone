63939a23f49d8535580ad32dad523e9ecd1f386b
enMista <oluwa.damilola246@gmail.com>
2025-12-31 15:15:41 -0500

    navbar finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,49 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import UserAccountnav from "./UserAccountnav";
 import NavbarItem from "./NavbarItem";
+import { BsChevronDown, BsSearch, BsBell } from "react-icons/bs";
+import MobileMenu from "./MobileMenu";
+import { useEffect, useState } from "react";
+import { useCallback } from "react";
+import AccountMenu from "./AccountMenu";
+
+const TOP_OFFSET = 66;
 
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
 
   return (
     <div className="py-4 fixed w-full z-40 top-0 bg-gradient-to-b from-black to-transparent">
-      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+      <div
+        className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${
+          showBackground ? "bg-zinc-900 bg-opacity-90" : ""
+        }`}
+      >
         <Link href="/">
           <img
             src="/images/logo.png"
@@ -35,14 +71,41 @@ const Navbar = async () => {
           <NavbarItem label="My List" />
           <NavbarItem label="Browse by languages" />
         </div>
-        <div className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"></div>
-        {session?.user ? (
-          <UserAccountnav />
-        ) : (
-          <Link href="/sign-in" className={buttonVariants()}>
-            Sign in
-          </Link>
-        )}
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
+        >
+          <p className="text-white text-sm">Browse</p>
+          <BsChevronDown
+            className={`text-white transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
+          <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsBell />
+          </div>
+
+          <div
+            onClick={toggleAccountMenu}
+            className="flex flex-row items-center gap-2 cursor-pointer relative"
+          >
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/default-blue.png" alt="Profile" />
+            </div>
+            <BsChevronDown
+              className={`text-white transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
+          </div>
+        </div>
       </div>
     </div>
   );
